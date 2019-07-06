9e8ced64b229f2491cea388f273ddf7f74ed7496
lucas0ray <39581458+lucas0ray@users.noreply.github.com>
2019-07-06 16:46:46 +0800

    Add files via upload

---
+++ javadata/web/js/read-xml.js
@@ -0,0 +1,29 @@
+$(function(){
+    $("#input-button").click(function(){
+        var javadeta=$("#input-text").val()
+        alert(javadeta)
+        $.ajax({
+            type: "post",
+            url: "xml/css.xml",
+            dataType: "xml",
+            success: function (xml) {
+                alert(javadeta)
+                $(xml).find("root").each(function() {
+                    var root = $(this);
+                    var name = root.attr("name")
+                    alert(name)
+                    if($.trim(name) == $.trim(javadeta)){
+                        var title = root.find("title").text();
+                        var format = root.find("format").text();
+                    }
+                }) 
+            }
+        })
+        
+      })
+
+
+
+
+
+})
