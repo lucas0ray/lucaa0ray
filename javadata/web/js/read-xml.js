a56efad0a2b7281c1e11704cf05f640aa8becc3b
lucas0ray <39581458+lucas0ray@users.noreply.github.com>
2019-07-06 17:47:00 +0800

    Update read-xml.js

--- javadata/web/js/read-xml.js
+++ javadata/web/js/read-xml.js
@@ -2,28 +2,52 @@ $(function(){
     $("#input-button").click(function(){
         var javadeta=$("#input-text").val()
         alert(javadeta)
-        $.get({
+        $("#div-01").empty()
+
+      
+        
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        $.ajax({
             type: "get",
-            url: "javadata/web/xml/css.xml",
+            url: "xml/css.xml",
             dataType: "xml",
             success: function (xml) {
                 alert(javadeta)
-                $(xml).find("part").each(function() {
-                    var part = $(this);
-                    var name = part.attr("name")
+                $(xml).find("root").each(function() {
+                    var root = $(this);
+                    var name = root.attr("name")
                     alert(name)
                     if($.trim(name) == $.trim(javadeta)){
-                        var title = part.find("title").text();
-                        var format = part.find("format").text();
+                        var title = root.find("title").text();
+                        var format = root.find("format").text();
+                        var html ='<div class="row">'
+                        +'<div class="col-md-6">#{title}</div>'
+                        +'<div class="col-md-6">#{format}</div>'
+                        +'</div>'
+                        html = html.replace(/#{title}/g,title)
+                        html = html.replace(/#{format}/g,format)
+                        $("#div-01").append(html)
                     }
                 }) 
             }
         })
-        
       })
-
-
-
-
-
 })
