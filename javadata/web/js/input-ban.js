1a78317ea86f4f961ca9feb2e79bbb90f6d8566a
lucas0ray <39581458+lucas0ray@users.noreply.github.com>
2019-07-07 12:15:50 +0800

    Add files via upload

---
+++ javadata/web/js/input-ban.js
@@ -0,0 +1,42 @@
+var filename
+function onclick0 (x,judge){
+    $("#div-input input").removeAttr("disabled")
+    $(x).attr("disabled", "disabled")
+    $('#input-button').removeAttr("disabled")
+
+    switch(judge) {
+        case 1:
+            filename="java"
+            $("#div_span_prompt>span").hide()
+            break;
+        case 2:
+            filename="javascript"
+            $("#div_span_prompt>span").hide()
+            break;
+        case 3:
+            filename="css"
+            $("#div_span_prompt>span").hide()
+            break;
+        case 4:
+            filename="html"
+            $("#div_span_prompt>span").hide()
+            break;
+        case 5:
+            filename="sql"
+            $("#div_span_prompt>span").hide()
+            break;
+        default:
+            $('#input-button').attr("disabled", "disabled")
+            $("#div_span_prompt>span").show()
+            break;
+    }
+}
+//多个js文件方法相互调，设置成全局方法 file_name.a()
+// var file_name
+// (function(){
+//     file_name = {
+//         a:function(){
+//             return filename
+//         }
+//     }
+// })()
---
+++ javadata/web/js/search.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    $("#input-button").click(function(){
+
+        var javadeta=new RegExp($("#input-text").val())
+        var html ='<table class="table table-bordered"><tr><th class="text-center">介绍</th><th class="text-center">代码</th></tr></table>'
+
+        $("#div-01").empty()
+        $("#div-01").append(html)
+        
+        read_xml.xml()
+        
+    })
+    
+})
