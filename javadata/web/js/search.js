95a9c03c11180b8d7ec8bdb1bfd9f3d840b25649
lucas0ray <39581458+lucas0ray@users.noreply.github.com>
2019-07-07 12:59:35 +0800

    Update search.js

--- javadata/web/js/search.js
+++ javadata/web/js/search.js
@@ -1,16 +1,31 @@
 var javadeta
+var html_table ='<table class="table table-bordered"><tr><th class="text-center">介绍</th><th class="text-center">代码</th></tr></table>'
 
 $(function(){
-
+    
     $("#input-button").click(function(){
         javadeta=new RegExp($("#input-text").val())
-        var html ='<table class="table table-bordered"><tr><th class="text-center">介绍</th><th class="text-center">代码</th></tr></table>'
-
-        $("#div-01").empty()
-        $("#div-01").append(html)
+        
         
         read_xml.xml()
         
     })
     
 })
+
+var empty_div_01
+
+(function(){
+
+    empty_div_01={
+
+        empty_table:function(){
+
+            $("#div-01").empty()
+            $("#div-01").append(html_table)
+            
+        }
+
+    }
+
+})()
